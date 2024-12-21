505af1fc18453a72881b15ca39a668a3ec882e03
paulo <paulofvilarimda@gmail.com>
2024-12-21 19:04:29 -0300

    update neo tree explorer keymaps

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -12,6 +12,23 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     cmd = "Neotree",
+    keys = {
+      {
+        "-",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "_",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>e", "-", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    },
     opts = function(_, opts)
       opts.window.position = "float"
       opts.window.mappings = {
@@ -55,27 +72,27 @@ return {
     "catgoose/nvim-colorizer.lua",
     event = "BufReadPre",
   },
-  {
-    "stevearc/oil.nvim",
-    config = function()
-      function _G.get_oil_winbar()
-        local dir = require("oil").get_current_dir()
-        if dir then
-          return vim.fn.fnamemodify(dir, ":~")
-        else
-          -- If there is no current directory (e.g. over ssh), just show the buffer name
-          return vim.api.nvim_buf_get_name(0)
-        end
-      end
-      require("oil").setup({
-        win_options = {
-          winbar = "%!v:lua.get_oil_winbar()",
-        },
-        view_options = {
-          show_hidden = true,
-        },
-      })
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
-  },
+  -- {
+  --   "stevearc/oil.nvim",
+  --   config = function()
+  --     function _G.get_oil_winbar()
+  --       local dir = require("oil").get_current_dir()
+  --       if dir then
+  --         return vim.fn.fnamemodify(dir, ":~")
+  --       else
+  --         -- If there is no current directory (e.g. over ssh), just show the buffer name
+  --         return vim.api.nvim_buf_get_name(0)
+  --       end
+  --     end
+  --     require("oil").setup({
+  --       win_options = {
+  --         winbar = "%!v:lua.get_oil_winbar()",
+  --       },
+  --       view_options = {
+  --         show_hidden = true,
+  --       },
+  --     })
+  --     vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+  --   end,
+  -- },
 }
